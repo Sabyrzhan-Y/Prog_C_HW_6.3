d15c1e925cca9d5f0b53defca241ebdf496c8e45
Sabyrzhan <144157934+Sabyrzhan-Y@users.noreply.github.com>
2024-01-03 14:16:53 +0500

    Задача 4. Выполняется

--- HW_6-3-4.c
+++ HW_6-3-4.c
@@ -6,6 +6,15 @@
 
 int main(void)
 {
-	
+	int a;
+	scanf("%d", &a);
+	if (a == 12 || a < 3)
+		printf("winter\n");
+	if (a > 2 && a < 6)
+		printf("spring\n");
+	if (a > 5 && a < 9)
+		printf("summer\n");
+	if (a > 8 && a < 12)
+		printf("autumn\n");
 	return 0;
 }
