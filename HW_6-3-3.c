ce6f7142389340429d3f884a378469aee697416e
Sabyrzhan <144157934+Sabyrzhan-Y@users.noreply.github.com>
2024-01-03 13:20:32 +0500

    Задача 3. Выполняется!

--- HW_6-3-3.c
+++ HW_6-3-3.c
@@ -1,3 +1,16 @@
 /* Задача 3. В порядке возрастания
 Данные на входе: Три целых числа
 Данные на выходе: Одно слово YES или NO */
+
+#include <stdio.h>
+
+int main(void)
+{
+	int a, b, c;
+	scanf("%d%d%d", &a, &b, &c);
+    if (a <= b && b <= c)
+		printf("YES\n");
+    else 
+        printf("NO\n");
+	return 0;
+}
