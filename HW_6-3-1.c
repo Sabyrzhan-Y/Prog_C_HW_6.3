6e5de5a395865186fcfb6e059e7fe6a124cb8061
Sabyrzhan <144157934+Sabyrzhan-Y@users.noreply.github.com>
2024-01-03 12:41:02 +0500

    Добавили переменную max и printf

--- HW_6-3-1.c
+++ HW_6-3-1.c
@@ -8,6 +8,10 @@ int main(void)
 {
 	int a, b, c, d, e;
 	scanf("%d%d%d%d%d", &a, &b, &c, &d, &e);
-	
+	int max = a > b ? a : b;
+		max = max > c ? max : c;
+		max = max > d ? max : d;
+		max = max > e ? max : e;
+		printf("%d", max);
 	return 0;
 }
